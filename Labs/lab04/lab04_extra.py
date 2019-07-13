6406864722239a672b6b8688360551bff48f98c6
sgalal <32255369+chromezh@users.noreply.github.com>
2019-07-13 15:22:09 -0700

    Emplace protogenic labs 4 and 5

---
+++ Labs/lab04/lab04_extra.py
@@ -0,0 +1,26 @@
+""" Lab 04 Optional Questions """
+
+from lab04 import *
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+
+def key_of_min_value(d):
+    """Returns the key in a dict d that corresponds to the minimum value of d.
+    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
+    >>> min(letters)
+    'a'
+    >>> key_of_min_value(letters)
+    'c'
+    """
+    "*** YOUR CODE HERE ***"
